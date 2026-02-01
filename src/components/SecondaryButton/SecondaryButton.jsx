59b846ad9d20d01fd9f3cd26bc1659cf0c3af12a
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2026-02-01 16:08:31 +0200

    minor fix

--- src/components/SecondaryButton/SecondaryButton.jsx
+++ src/components/SecondaryButton/SecondaryButton.jsx
@@ -1,12 +1,12 @@
 import css from './SecondaryButton.module.css';
 
 export default function SecondaryButton({ text, onClick, ref = '' }) {
-  return ref !== '' ? (
-    <button ref={ref} type="button" onClick={onClick} className={css.btn}>
+  return ref === '' ? (
+    <button type="button" onClick={onClick} className={css.btn}>
       {text}
     </button>
   ) : (
-    <button type="button" onClick={onClick} className={css.btn}>
+    <button ref={ref} type="button" onClick={onClick} className={css.btn}>
       {text}
     </button>
   );
